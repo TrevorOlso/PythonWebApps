05e5b0401f43c82522e75d9397fca91095875766
Trevor <tdolson444@gmail.com>
2024-09-23 13:16:47 -0700

    added 2 more superheros everything is working

--- ClassroomDemos/ClassroomDemos/Superhero/config/urls.py
+++ ClassroomDemos/ClassroomDemos/Superhero/config/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from hero.views import MainView, HulkView, IronManView, BlackWidowView
+from hero.views import MainView, HulkView, IronManView, BlackWidowView, AntManView, BlackPantherView
 
 urlpatterns = [
     path('',        MainView.as_view()),
     path('hulk',        HulkView.as_view()),
     path('ironman', IronManView.as_view()),
     path('home', MainView.as_view()),
-    path('blackwidow', BlackWidowView.as_view())
+    path('blackwidow', BlackWidowView.as_view()),
+    path('blackpanther', BlackPantherView.as_view()),
+    path('antman', AntManView.as_view()),
+
 ]
--- ClassroomDemos/ClassroomDemos/Superhero/hero/views.py
+++ ClassroomDemos/ClassroomDemos/Superhero/hero/views.py
@@ -47,3 +47,29 @@ class HulkView(TemplateView):
             'weakness1': 'Arrogant',
             'weakness2': 'Has to take meds to make strong'
         }
+class AntManView(TemplateView):
+    template_name = 'heros.html'
+
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Antman',
+            'id': 'Scott Lang',
+            'image': 'static/images/antman.jpg',
+            'strength1': 'Can change size',
+            'strength2': 'Super Strength',
+            'weakness1': 'Dealing with his past',
+            'weakness2': 'Suit malfunctions at important times'
+        }
+class BlackPantherView(TemplateView):
+    template_name = 'heros.html'
+
+    def get_context_data(self, **kwargs):
+        return{
+            'title': 'Black Panther',
+            'id': 'TChalla',
+            'image': 'static/images/blackpanther.jpg',
+            'strength1': 'Super speed',
+            'strength2': 'nearly invincible',
+            'weakness1': 'Arrogant',
+            'weakness2': 'Is mortal'
+        }
