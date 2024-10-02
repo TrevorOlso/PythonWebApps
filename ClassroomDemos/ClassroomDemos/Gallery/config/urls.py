b99b30f5574122c972fb4b16c3087c37a5dc3310
Trevor <tdolson444@gmail.com>
2024-10-02 11:33:43 -0700

    finished implementing the Superhero_database

--- ClassroomDemos/ClassroomDemos/Gallery/config/urls.py
+++ ClassroomDemos/ClassroomDemos/Gallery/config/urls.py
@@ -1,10 +1,12 @@
 from django.views.generic import RedirectView
 from django.urls import path
+from django.contrib import admin
 
 from photos.views import PhotoDetailView, PhotoListView
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
 
     # Home
     path('', RedirectView.as_view(url='photo/')),
